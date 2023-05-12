04cde385f81c98496c82d529e08b19415d140117
Santhoshkumar Nagulanchi <santhoshkumarnagulanchi@Santhoshkumars-MacBook-Pro.local>
2023-05-12 08:03:57 +0800

    couchbase as a backeend for spring config cloud

--- src/main/java/com/sannette/eis/mega/SpringCloudConfigCouchbaseApplication.java
+++ src/main/java/com/sannette/eis/mega/SpringCloudConfigCouchbaseApplication.java
@@ -2,8 +2,9 @@ package com.sannette.eis.mega;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
+@EnableConfigServer
 public class SpringCloudConfigCouchbaseApplication {
 
 	public static void main(String[] args) {
